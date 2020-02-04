6cfdb1f79d98f98e1c971887e56f30e7a5268341
jtw22@students.ptcollege.edu <>
2020-02-04 12:13:02 -0500

    This is the first commit.

---
+++ src/github/repos/GitHubRepos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github.repos;
+
+/**
+ *
+ * @author jtw22
+ */
+public class GitHubRepos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Wilson();
+    }
+    public static void Wilson(){
+        System.out.println("Jacob Wilson");
+    }
+    
+}
